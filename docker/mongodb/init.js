6e1644b32e21b2dd04a927a378bd4df6c251e4e2
Jonah987654321 <jonah.emme@web.de>
2026-09-10 23:20:09 +0200

    fix: update init script to create replica set

--- docker/mongodb/init.js
+++ docker/mongodb/init.js
@@ -1,10 +1,23 @@
+const REPLSET_HOST = "mongodb_host:27017";
+
+try {
+  rs.status();
+} catch (e) {
+  rs.initiate({ _id: "rs0", members: [{ _id: 0, host: REPLSET_HOST }] });
+}
+
+while (!db.hello().isWritablePrimary) {
+  sleep(200);
+}
+
+db.getSiblingDB("admin").createUser({
+  user: process.env.MONGO_ROOT_USERNAME,
+  pwd: process.env.MONGO_ROOT_PASSWORD,
+  roles: [{ role: "root", db: "admin" }],
+});
+
 db.createUser({
   user: process.env.MONGO_USER_NAME,
   pwd: process.env.MONGO_USER_PWD,
-  roles: [
-    {
-      role: "readWrite",
-      db: process.env.MONGO_INITDB_DATABASE 
-    }
-  ]
+  roles: [{ role: "readWrite", db: process.env.MONGO_INITDB_DATABASE }],
 });
